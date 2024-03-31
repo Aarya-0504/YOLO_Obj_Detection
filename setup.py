cd866ecc91938d8e6b9f6cc31e659bab835fb801
Aarya_P <aarya.patil@spit.ac.in>
2024-03-31 16:23:45 +0530

    template, environment, modules

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Aarya Patil',
+    author_email= 'aaryappatil1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
